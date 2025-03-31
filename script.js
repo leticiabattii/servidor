3246d3d5dc0b722161a99461e380e41fb787d413
LETÍCIA ABATTI VAZ <leticia_a_vaz@estudante.sesisenai.org.br>
2025-03-31 11:20:10 -0300

    atualizacao antes da aula acabar

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
 const http = require('http')
+const url = require('url')
 const {soma} = require('./teste')
 
 const PORT = 8000
 
 //criar servidor
 const server = http.createServer((req, res) =>{
-    let valores = req.url
-        .replace('/', '')
-        .replace('?', '')
-        .split('&')
-    let parametros ={}
-    for (item in valores) {
-        let chaveValor = valores[item].split('=')
-        parametros[chaveValor[0]] = Number(chaveValor[1])
-    }
-    let resultado = soma(parametros.a,parametros.b)
+    const urlCapturada = url.parse(req.url, true)
+    const {query} = urlCapturada
+    let resultado = soma(Number(query.a), Number(query.b))
     res.end(`o valor final é ${resultado}`)
 })
 
