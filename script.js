42b5f066f8f3071e6fd8b38e38bdbcd758bd94c8
LETÍCIA ABATTI VAZ <leticia_a_vaz@estudante.sesisenai.org.br>
2025-04-02 10:48:06 -0300

    adiconando gitignore

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 const http = require('http')
 const url = require('url')
-const {soma} = require('./teste')
+const {soma, subtracao,multiplicacao,divisao} = require('./teste')
 
 const PORT = 8000
 
 //criar servidor
 const server = http.createServer((req, res) =>{
     const urlCapturada = url.parse(req.url, true)
-    const {query} = urlCapturada
-    let resultado = soma(Number(query.a), Number(query.b))
-    res.end(`o valor final é ${resultado}`)
-})
+    console.log(urlCapturada)
+    const {query, pathname} = urlCapturada
+    let resultado = 0
+    if(pathname== '/soma'){
+        resultado = soma(Number(query.a), Number(query.b))
+        res.end(`o valor final é ${resultado}`)
+    }else if(pathname == '/subtracao'){
+        resultado = subtracao(Number(query.a), Number(query.b))
+    }else if(pathname == '/divisao'){
+        resultado = divisao(Number(query.a), Number(query.b))
+   
+   }else if(pathname == '/multiplicacao'){
+    resultado = multiplicacao(Number(query.a), Number(query.b))
+   }else{
+    res.statusCode = 404
+    res.end('Pagina nao encontrada')
+   }
+   res.end(`O valor final e ${resultado}`)
+}
+    )
 
 server.listen(PORT, ()=> {
     console.log(`Servidor rodando na porta ${PORT}`)
--- teste.js
+++ teste.js
@@ -2,5 +2,18 @@ function soma (a, b){
     return a + b;
 }
 
+function subtracao (a, b){
+    return a - b;
+}
+
+function divisao(a,b){
+    return a/b
+}
+
+function multiplicacao(a,b){
+    return a*b
+}
+
 
 module.exports = {soma}
+module.exports = {subtracao,soma,divisao,multiplicacao}
